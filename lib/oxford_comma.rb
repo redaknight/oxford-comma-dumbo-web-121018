1331e46ddb717c8c9b56f8dd99548127ebef9740
AL FATAH KADER <alfatah.kader@gmail.com>
2018-12-05 02:56:01 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
 
+if array.count == 1
+    array.join(",")
+    
+  elsif array.count == 2
+        array.join(" and ")
+  
+  elsif array.count >= 3
+         array[0..-2].join(", ") + ", and " + array[-1]
+    
+   end
+   
 end
